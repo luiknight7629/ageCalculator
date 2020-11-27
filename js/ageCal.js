0c474fe8ac86c5f05b5718435e558b9fdb43e948
luiknight7629 <70377881+luiknight7629@users.noreply.github.com>
2020-11-27 21:51:59 +0800

    Add files via upload

---
+++ js/ageCal.js
@@ -0,0 +1,15 @@
+
+function getCal(){
+var yourAge = document.getElementById("ageCal").value;
+var displaying = document.getElementById('output');
+displaying.style.display = 'block';
+var leftAge = 90 - yourAge;
+var day = 365* leftAge;
+var week = 52*leftAge;
+var month = 12*leftAge;
+
+
+document.getElementById("output").innerHTML= "Days: "+day+"<br>Weeks: "+ week+"<br>Months: "+month+"<br>left to reach 90 years old";
+
+
+}
